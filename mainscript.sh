5e70b6be1b5962cb891baa7d4227b18ac3c0fe4f
Alavi Khan <leonasting@gmail.com>
2022-10-12 07:03:02 -0700

    Create mainscript.sh

---
+++ mainscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=policy_train
+#SBATCH --output=pl2ap-srun.log
+#
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=0:01:00
+#SBATCH --mem-per-cpu=1000
+#SBATCH --mail-user=alavi.khan@sjsu.edu
+#SBATCH --mail-type=END
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+python run_ddpg.py --expID 3 --video_count 0 --n_cycles 40000 --chunk 10 --point_cloud --pointnet_load_path 2 --no_save_buffer --load_path 3 --n_test_rollouts 24 --num_rollouts 24 --num_parallel_envs 24
+
