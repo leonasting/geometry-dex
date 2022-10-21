0ca11f1acdf0d72d5b9a1941ab96b2a0b1d7c6b0
Alavi Khan <leonasting@gmail.com>
2022-10-21 15:13:50 -0700

    Update mainscript.sh

--- mainscript.sh
+++ mainscript.sh
@@ -5,7 +5,7 @@
 #
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=6
 #SBATCH --time=5-10:01:00
 #SBATCH --mem-per-cpu=2000
 #SBATCH --mail-user=alavi.khan@sjsu.edu
@@ -13,4 +13,5 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
-python run_ddpg.py --expID 3 --video_count 0 --n_cycles 40000 --chunk 10 --point_cloud --pointnet_load_path 2 --no_save_buffer --load_path 3 --n_test_rollouts 60 --num_rollouts 60 --num_parallel_envs 60
+conda activate om
+python /home/015898207/geometry-dex/run_ddpg.py --expID 3 --video_count 0 --n_cycles 40000 --chunk 10 --point_cloud --pointnet_load_path 2 --no_save_buffer --eval --load_path 3 --num_parallel_envs 60 --num_rollouts 60 --n_test_rollouts 60
