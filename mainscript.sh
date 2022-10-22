42a29b419c746bb2fff4a4c35556852206ac959e
Alavi Khan <leonasting@gmail.com>
2022-10-22 08:46:35 -0700

    Update mainscript.sh

--- mainscript.sh
+++ mainscript.sh
@@ -13,5 +13,9 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
+echo $PWD > /home/015898207/pwd_value.txt
+echo "started_running" > /home/015898207/pwd_value.txt
 conda activate om
-python /home/015898207/geometry-dex/run_ddpg.py --expID 3 --video_count 0 --n_cycles 40000 --chunk 10 --point_cloud --pointnet_load_path 2 --no_save_buffer  --load_path 3 --num_parallel_envs 60 --num_rollouts 60 --n_test_rollouts 60
+echo "activated om" > /home/015898207/pwd_value.txt
+python run_ddpg.py --expID 3 --video_count 0 --n_cycles 40000 --chunk 10 --point_cloud --pointnet_load_path 2 --no_save_buffer   --num_parallel_envs 60 --num_rollouts 60 --n_test_rollouts 60 --fresh
+echo "done_running" > /home/015898207/pwd_value.txt
