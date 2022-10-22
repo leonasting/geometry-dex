4155caa3f98a8029f5ea6281cc7505ecf4c953b5
Alavi Khan <leonasting@gmail.com>
2022-10-22 08:29:33 -0700

    Create main2.sh

---
+++ main2.sh
@@ -0,0 +1,17 @@
+!/bin/bash
+#
+#SBATCH --job-name=policy_train
+#SBATCH --output=pl2ap-srun.log
+#
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=6
+#SBATCH --time=5-10:01:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --mail-user=alavi.khan@sjsu.edu
+#SBATCH --mail-type=END
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+
+echo $PWD > /home/015898207/pwd_value.txt
