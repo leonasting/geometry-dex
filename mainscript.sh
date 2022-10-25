88a13fb2fd7dc67bc5684f31eb10fad1e65c5185
Alavi Khan <leonasting@gmail.com>
2022-10-24 17:15:23 -0700

    Update mainscript.sh

--- mainscript.sh
+++ mainscript.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=6
 #SBATCH --time=5-10:01:00
-#SBATCH --mem-per-cpu=2000
+#SBATCH --mem-per-cpu=12000
 #SBATCH --mail-user=alavi.khan@sjsu.edu
 #SBATCH --mail-type=END
 #SBATCH --partition=gpu
