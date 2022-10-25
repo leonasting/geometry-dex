0cacaf8ad4900d7be6cf26f9981562c5f6f931bd
Alavi Khan <leonasting@gmail.com>
2022-10-24 22:11:38 -0700

    Update mainscript.sh

--- mainscript.sh
+++ mainscript.sh
@@ -5,7 +5,7 @@
 #
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=4
 #SBATCH --time=5-10:01:00
 #SBATCH --mem-per-cpu=12000
 #SBATCH --mail-user=alavi.khan@sjsu.edu
@@ -17,5 +17,5 @@ echo $PWD > /home/015898207/pwd_value.txt
 echo "started_running" > /home/015898207/pwd_value.txt
 conda activate om
 echo "activated om" > /home/015898207/pwd_value.txt
-python run_ddpg.py --expID 3 --video_count 0 --n_cycles 40000 --chunk 10 --point_cloud --pointnet_load_path 2 --no_save_buffer   --num_parallel_envs 60 --num_rollouts 60 --n_test_rollouts 60 --fresh
+python run_ddpg.py --expID 2 --video_count 0 --n_cycles 40000 --chunk 10 --point_cloud --pointnet_load_path 2 --no_save_buffer   --num_parallel_envs 60 --num_rollouts 60 --n_test_rollouts 60 --fresh
 echo "done_running" > /home/015898207/pwd_value.txt
