d67068e620d3065ec012efd5be0b0a174160e583
Alavi Khan <leonasting@gmail.com>
2022-10-28 16:12:52 -0700

    Update mainscript.sh

--- mainscript.sh
+++ mainscript.sh
@@ -15,5 +15,5 @@
 
 echo $PWD > /home/015898207/pwd_value.txt
 echo "started_running" > /home/015898207/pwd_value.txt
-python run_ddpg.py --expID 4 --video_count 0 --n_cycles 40000 --chunk 10 --point_cloud --pointnet_load_path 2 --no_save_buffer   --num_parallel_envs 6 --num_rollouts 6 --n_test_rollouts 6 --fresh
+python run_ddpg.py --train_names cube --test_names cube --expID 5 --video_count 0 --n_cycles 40000 --chunk 10 --point_cloud --pointnet_load_path 2 --no_save_buffer  --num_parallel_envs 6 --num_rollouts 6 --n_test_rollouts 6
 echo "done_running" > /home/015898207/pwd_value.txt
