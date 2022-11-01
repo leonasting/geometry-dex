79a813991d1ab1a8d0a4c802b47cf85c9d008c39
Alavi Khan <leonasting@gmail.com>
2022-11-01 08:49:31 -0700

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name=pol_acup
+#SBATCH --output=srun_a_cups.log
+#
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=5-10:01:00
+#SBATCH --mem-per-cpu=12000
+#SBATCH --mail-user=alavi.khan@sjsu.edu
+#SBATCH --mail-type=END
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+echo $PWD > /home/015898207/pwd_value.txt
+echo "started_running" > /home/015898207/pwd_value.txt
+python run_ddpg.py --train_names a_cups --test_names a_cups --expID 8 --video_count 0 --n_cycles 40000 --chunk 10 --point_cloud --pointnet_load_path 2 --no_save_buffer  --num_parallel_envs 6 --num_rollouts 6 --n_test_rollouts 6
+echo "done_running" > /home/015898207/pwd_value.txt
